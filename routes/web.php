66ca94c5854696957ea3189358ddcb1e043349e4
mohamad.nematizadeh <89206040+Mohammadnematizade@users.noreply.github.com>
2022-08-12 14:11:47 +0430

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CityController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CommentController;
+
 
 
 
@@ -23,8 +25,11 @@ use App\Http\Controllers\ProductController;
 
 
 Route::get('/',[CartController::class, "index"]);
-Route::get('/products',[CartController::class, "orders"])->name('products.order');
+Route::get('/products',[ProductController::class, "get_all_for_client"])->name('products.order');
 Route::get('/product',[CartController::class, "order"])->name('product.order');
+Route::get('/product/{id}',[ProductController::class, "index"] );
+Route::post('/product-info',[ProductController::class, "product_info"]);
+
 Route::get('/admin',[AdminController::class, "index"]);
 Route::get('/profile',[UserController::class, "profile"]);
 Route::get('/login',[UserController::class, 'login_get']);
@@ -36,7 +41,7 @@ Route::post('/register',[UserController::class, 'register_post']);
 Route::get('/logout',[UserController::class,'logout']);
 Route::get('/admin_dashboard',[AdminController::class, "admin_get"]);
 Route::post('/admin_dashboard',[AdminController::class, "admin_post"]);
-Route::get('/profile_amin',[AdminController::class, "admin_profile"]);
+Route::get('/profile_amin',[AdminController::class, "admin_profile"])->middleware("auth");
 Route::get('/admin_logout',[AdminController::class, "admin_logout"]);
 
 
@@ -47,15 +52,17 @@ Route::get('admin/cities/edit/{id}', [CityController::class, 'edit_get']);
 Route::post('admin/cities/edit', [CityController::class, 'edit_post']);
 Route::get('admin/cities/delete/{id}', [CityController::class, 'delete']);
 //product
-Route::get('/admin/product',[CityController::class, 'all']);
-//add
-Route::get('/admin/product/add', [CityController::class, 'add_pr_get']);
-Route::post('/admin/product/add', [CityController::class, 'add_pr_post']);
-//edit
-Route::get('/admin/product/edit/{id}', [CityController::class, 'edit_pr_get']);
-Route::post('/admin/product/edit', [CityController::class, 'edit_pr_post']);
-//delete
-Route::get('admin/product/delete/{id}', [CityController::class, 'ddelete']);
+Route::get('/admin/products',[ProductController::class, "get_all_for_admin"]);
+Route::get('/admin/products/add', [ProductController::class, 'add_get']);
+Route::post('/admin/products/add', [ProductController::class, 'add_post']);
+Route::get('admin/products/edit/{id}', [ProductController::class, 'edit_get']);
+Route::post('admin/products/edit', [ProductController::class, 'edit_post']);
+Route::get('admin/products/delete/{id}', [ProductController::class, 'delete']);
+
+Route::post('/send-comment',[CommentController::class,'send_comment']);
+
+
+
 
 
 Route::get('/admin/users', function () {
@@ -66,4 +73,8 @@ Route::get('/admin/comment', [ProductController::class, 'comments']);
 
 Route::get('/admin/de', function () {
     return view('dashoard');
+});
+
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
 });
