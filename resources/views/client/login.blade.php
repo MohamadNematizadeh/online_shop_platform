2a0ff08dde22770337d1807498270fd95a64a232
mohamad.nematizadeh <89206040+Mohammadnematizade@users.noreply.github.com>
2022-08-12 14:13:02 +0430

    Update login.blade.php

--- resources/views/client/login.blade.php
+++ resources/views/client/login.blade.php
@@ -6,6 +6,11 @@
 <div class="container">
     <div class="row">
         <div class="col">
+            <style>
+                form {
+                    text-align: right;
+                }
+            </style>
             <form method="post" action="{{url("/login")}}">
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
@@ -16,8 +21,8 @@
                     <input type="password" class="form-control"name="password" >
                 </div>
                 <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    <input type="checkbox" class="form-check-input" name="remember_me" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">یاداوری نام کاربری</label>
                 </div>
                 @csrf
                 <button type="submit" class="btn btn-primary">ورود</button>
